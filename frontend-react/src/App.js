325b77beeefe2723431c29c0821de9c542c6fcae
Alvaro Torro <a.torro@shakersworks.com>
2025-05-23 16:01:28 +0200

    login fixed

--- frontend-react/src/App.js
+++ frontend-react/src/App.js
@@ -3,8 +3,12 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from './components/HomePage';
 import ChatPage from './components/ChatPage';
 import FeedbackPage from './components/FeedbackPage';
+import LoginPage from './components/LoginPage';
 
 function App() {
+  if (localStorage.getItem("v-account") === null) {
+    return <LoginPage />;
+  }
   return (
     <Router>
       <Routes>
